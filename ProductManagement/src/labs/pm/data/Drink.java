1b232be44f9c8ac9acb6124712491ee51476ca61
Andrei <mygrandstandproject@gmail.com>
2021-03-29 05:30:11 +0300

    Update Drink.java
    File refactoring.
    Attention! It is possible to transfer some methods to the parent class!

--- ProductManagement/src/labs/pm/data/Drink.java
+++ ProductManagement/src/labs/pm/data/Drink.java
@@ -1,7 +1,7 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
-
+import static java.math.BigDecimal.valueOf;
 /**
  * <p>ProductManagement Created by Home Work Studio AndrHey [andreigp]</p>
  * <p>FileName: Drink.java</p>
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 
 public class Drink extends Product {
     /**
-     * This constructor is used to create an object of an existing product obtained from the database.
+     * <h2>This constructor is used to create an object of an existing product obtained from the database.</h2>
      * @param id int
      * @param name String
      * @param price BigDecimal
@@ -22,5 +22,38 @@ public class Drink extends Product {
      */
     public Drink(int id, String name, BigDecimal price, Rating rating, BigDecimal DISCOUNT_RATE) {
         super(id, name, price, rating, DISCOUNT_RATE);
+    }
+
+    /**
+     * <h2>Basic constructor for creating a new product.</h2>
+     * <p>It is used to create and save a new unit of product in the database, with a preliminary percentage
+     * and set the default primary rating (Not NOT_RATED).</p>
+     * <p>When creating a new product, a unique product ID[SKU] is generated and assigned</p>
+     * @param name String - The name of the priduct being created
+     * @param price BigDecimal - Primary product interest
+     * @param rating BigDecimal - The primary rating given to a new product
+     */
+    public Drink(String name, BigDecimal price, Rating rating) {
+        super(name, price, rating);
+    }
+
+    @Override
+    public Drink applyName(String name) {
+        return new Drink(this.getId(), name, this.getPrice(), this.getRating(), this.getDiscount());
+    }
+
+    @Override
+    public Drink applyPrice(BigDecimal price) {
+        return new Drink(this.getId(), this.getName(), price, this.getRating(), this.getDiscount());
+    }
+
+    @Override
+    public Drink applyRating(Rating rating) {
+        return new Drink(this.getId(), this.getName(), this.getPrice(), rating, this.getDiscount());
+    }
+
+    @Override
+    public Drink applyDiscountRate(int rate) {
+        return new Drink(this.getId(), this.getName(), this.getPrice(), this.getRating(), valueOf(rate));
     }
 }
