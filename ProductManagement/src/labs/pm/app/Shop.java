18c688356ac801a7bd4f3fa5df6040dd3b6fa152
Andrei <mygrandstandproject@gmail.com>
2021-04-09 16:49:37 +0300

    Update Shop.java
    Testing new fituries

--- ProductManagement/src/labs/pm/app/Shop.java
+++ ProductManagement/src/labs/pm/app/Shop.java
@@ -4,6 +4,7 @@ import labs.pm.data.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Comparator;
 
 
 /**
@@ -17,9 +18,13 @@ import java.time.LocalDate;
 
 public class Shop {
     public static void main(String[] args) {
-        CommodityManager cm = new CommodityManager();
+        System.out.println("This app supports the following languages:");
+        CommodityManager.getSupportedLocales().forEach(System.out::println);
+        System.out.println();
+
+        CommodityManager cm = new CommodityManager("ru-RU");
         Product drink = cm.createNewProduct("Juice", 76.99, ProductType.DRINK);
-        cm.printProductReport(drink);
+//        cm.printProductReport(drink);
         drink = cm.reviewProduct(drink, Rating.TWO_STARS, "Бычья моча!");
         drink = cm.reviewProduct(drink, Rating.FOUR_STARS, "А сочок-то ничёвский!");
         drink = cm.reviewProduct(drink, Rating.FIVE_STARS, "Отличный сок!");
@@ -29,12 +34,12 @@ public class Shop {
         drink = cm.reviewProduct(drink, Rating.FOUR_STARS, "Пробовал лучше, но и этот норм!");
         drink = cm.reviewProduct(drink, Rating.FIVE_STARS, "Сегодня был свежевыжатый!");
         drink = cm.reviewProduct(drink, Rating.THREE_STARS, "Сегодня не свежевыжатый был!");
-        drink = cm.reviewProduct(drink, Rating.FOUR_STARS, "Выше среднего сок!");
-        cm.printProductReport(drink);
+        cm.reviewProduct(drink, Rating.FOUR_STARS, "Выше среднего сок!");
+//        cm.printProductReport(drink);
 
 
-        int FoodId = cm.createNewProduct("Cookie", 76.99, ProductType.FOOD).getId();
-        cm.printProductReport(FoodId);
+        int FoodId = cm.createNewProduct("Cookie", 66.99, ProductType.FOOD).getId();
+//        cm.printProductReport(FoodId);
         cm.reviewProduct(FoodId, Rating.TWO_STARS, "Пресное и крошится!");
         cm.reviewProduct(FoodId, Rating.THREE_STARS, "Только со сладким чаем можно есть!");
         cm.reviewProduct(FoodId, Rating.FOUR_STARS, "За такую цену пойдёт!");
@@ -45,10 +50,12 @@ public class Shop {
         cm.reviewProduct(FoodId, Rating.FIVE_STARS, "Голубей кормить самое то!");
         cm.reviewProduct(FoodId, Rating.THREE_STARS, "Как совдеповские галеты!");
         cm.reviewProduct(FoodId, Rating.TWO_STARS, "Вспомнил далёкое серое детство!");
-        cm.printProductReport(FoodId);
+//        cm.printProductReport(FoodId);
 
-        int nonFoodId = cm.createNewProduct("Fairy", 76.99, ProductType.FOOD).getId();
-        cm.printProductReport(nonFoodId);
+        cm.changeLocal("en-US");
+
+        int nonFoodId = cm.createNewProduct("Fairy", 79.99, ProductType.FOOD).getId();
+//        cm.printProductReport(nonFoodId);
         cm.reviewProduct(nonFoodId, Rating.FOUR_STARS, "Хорошее средство!");
         cm.reviewProduct(nonFoodId, Rating.THREE_STARS, "Я из Виллабаджо!");
         cm.reviewProduct(nonFoodId, Rating.FOUR_STARS, "За такую цену пойдёт!");
@@ -59,8 +66,17 @@ public class Shop {
         cm.reviewProduct(nonFoodId, Rating.FIVE_STARS, "С аллоэ прям лучше не найти!");
         cm.reviewProduct(nonFoodId, Rating.FIVE_STARS, "Я тоже из Вилларибо!");
         cm.reviewProduct(nonFoodId, Rating.FIVE_STARS, "Я даже похудела, как оно борется с жиром!");
-        cm.printProductReport(nonFoodId);
+//        cm.printProductReport(nonFoodId);
 
-        cm.printProductReport(1);
+//        cm.printProductReport(1);
+
+        Comparator<Product> ratingSorter = (p1, p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
+        Comparator<Product> priceSorter = (p1, p2) -> p2.getPrice().compareTo(p1.getPrice());
+        Comparator<Product> idSorter = (p1, p2) -> p1.getId() - p2.getId();
+        cm.printProducts(ratingSorter);
+        cm.printProducts(priceSorter);
+        cm.printProducts(idSorter);
+        cm.printProducts(ratingSorter.thenComparing(priceSorter));
+        cm.printProducts(ratingSorter.thenComparing(priceSorter).reversed());
     }
 }
